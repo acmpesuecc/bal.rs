40c6a96545e39920c3dacdd5aaf50ef046fa58ad
pratham-exe <prathamrao001@gmail.com>
2024-10-18 21:53:45 +0530

    Dumb mistake

--- install.sh
+++ install.sh
@@ -3,7 +3,8 @@
 echo "Installing rust"
 curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh -y
 echo "Rust installed successfully"
-rustc --version
+source $HOME/.cargo/env
 echo "Rust version"
+rustc --version
 echo "Cargo build"
 cargo build
