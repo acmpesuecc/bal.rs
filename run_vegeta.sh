9c55ce4e29fa55a9f44019882605625c62f95aa8
“mebin <mebin@Mebins-MacBook-Pro.local>
2024-10-19 02:54:22 +0530

    Pushed with vegeta.exe in directory by mistake

--- run_vegeta.sh
+++ run_vegeta.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo "GET http://127.0.0.1:8000" | ./vegeta.exe attack -rate="$1/s" -duration=1s | ./vegeta.exe report
+echo "GET http://127.0.0.1:8000" | ./vegeta attack -rate="$1/s" -duration=1s | ./vegeta report
 
